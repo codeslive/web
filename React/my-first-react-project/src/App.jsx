89e0c91121ead5867459cdb058142339fee73e0c
codesLive <2783779285@qq.com>
2023-06-13 21:04:11 +0800

    组件属性
    Signed-off-by: codesLive <2783779285@qq.com>

--- React/my-first-react-project/src/App.jsx
+++ React/my-first-react-project/src/App.jsx
@@ -1,10 +1,19 @@
-import ReportButton from "./components/ReportButton";
+import { useState } from "react";
+// import ReportButton from "./components/ReportButton";
 import Counter from "./components/Counter";
 function App () {
+
+  const [countValue, setCountValue] = useState(10);
+  const handleClick = () => {
+    setCountValue(prev => prev + 1);
+  }
+
   return (
     <div>
-      <ReportButton></ReportButton>
-      <Counter></Counter>
+      {/* <ReportButton></ReportButton> */}
+      <Counter defaultValue={countValue}></Counter>
+      <button onClick={handleClick}>点击我</button>
+      {/* <Counter></Counter> */}
     </div>
   )
 }
--- React/my-first-react-project/src/components/Counter/index.jsx
+++ React/my-first-react-project/src/components/Counter/index.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
-export default function Counter () {
+export default function Counter (props) {
   // 这会count的值变动了 它虽然是Counter内部的数据, 但是它凭什么能够让React去重新渲染组件呢
   // 我们要知道 函数组件内部的数据有很多种, 那我们要选能够通知React去重新渲染组件的数据
   // 那么我们需要在内部创造出一种数据, 这种数据可以和React产生直接关联, 从而使得当组件状态发生变化时, React可以重新更新页面
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(props.defaultValue || 0);
   const increase = () => {
     setCount(prev => prev + 1);
   }
@@ -16,7 +16,8 @@ export default function Counter () {
     <div>
       <button onClick={increase}>+</button>
       <span>{count}</span>
-      <button onClick={decrease}>-</button>
+      <button onClick={decrease}>-</button><br />
+      <span>{props.defaultValue}</span>
     </div>
   )
 }
