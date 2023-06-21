266037c07b91633ba293333db18a43f300449165
codesLive <2783779285@qq.com>
2023-06-21 20:36:12 +0800

    修改样式
    Signed-off-by: codesLive <2783779285@qq.com>

--- react-demo/src/pages/NotFound.jsx
+++ react-demo/src/pages/NotFound.jsx
@@ -6,7 +6,7 @@ export default function NotFound () {
         // 404 页面
         <Container>
           <h1>404</h1>
-          <img style={{ margin: "20px 0" }} src="https://images.codeslive.top/static/202306202337400.webp" alt="404" />
+          <img src="https://images.codeslive.top/static/202306202337400.webp" alt="404" />
         </Container>
       }
     </div >
