a00258403e1b878c37114bfaf551b0f501f638f6
codesLive <2783779285@qq.com>
2023-06-22 00:50:29 +0800

    666
    Signed-off-by: codesLive <2783779285@qq.com>

--- react-demo/src/pages/NotFound.jsx
+++ react-demo/src/pages/NotFound.jsx
@@ -1,12 +1,14 @@
 // 导入 404 页面的容器组件
 import Container from './Container.jsx';
 export default function NotFound () {
+
   return (
     <div>
       {
         // 404 页面
         <Container>
           <h1>404</h1>
+
           <img src="https://images.codeslive.top/static/202306202337400.webp" alt="404" />
         </Container>
       }
