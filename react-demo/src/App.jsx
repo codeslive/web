152c485373d962c4e73e0d95df38f8ff37455b10
codesLive <2783779285@qq.com>
2023-06-22 00:53:02 +0800

    累加器
    Signed-off-by: codesLive <2783779285@qq.com>

--- react-demo/src/App.jsx
+++ react-demo/src/App.jsx
@@ -2,8 +2,17 @@ import { useState } from 'react'
 function App() {
   const [count, setCount] = useState(0)
 
+  // 累加器
+  const increment = () => {
+    setCount(count + 1)
+  }
+
+
   return (
-    <div></div>
+    <div>
+      <h2>当前求和为：{count}</h2>
+      <button onClick={increment}>点我+1</button>
+    </div>
   )
 }
 
