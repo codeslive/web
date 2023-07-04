5b1ca47c2c54f118ee086697e7a7ee1d363c315b
codesLive <2783779285@qq.com>
2023-07-04 23:32:19 +0800

    @别名配置
    Signed-off-by: codesLive <2783779285@qq.com>

--- react-admin-demo/src/main.tsx
+++ react-admin-demo/src/main.tsx
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // 样式初始化
 import "reset-css";
+
+// UI 框架样式
+
+
+// 全局样式
+import "@/assets/styles/global.scss";
+
+// 组件样式
+
 import App from './App.tsx';
 
 
